471890d9b88493ad2dcfdcd7bbbcf17fa3e3737b
U-ta122 <yuta8038@gmail.com>
2024-08-26 14:38:47 +0900

    7-2-2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTIPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!--Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            @foreach ($posts as $toko)
+                <dev class="post">
+                    <h2 class="title">
+                        {{ $toko->title }}
+                    </h2>
+                    <p class='body'>{{ $toko->body }}</p>
+                    <p >{{ $toko->created_at }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='Paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
+
