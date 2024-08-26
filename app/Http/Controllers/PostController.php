b9d06c7b29a044bfa74f16fa4d3635f140258d50
U-ta122 <yuta8038@gmail.com>
2024-08-26 16:35:45 +0900

    7-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
 
 class PostController extends Controller
 {
        
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();//$postの中身を戻り値にする。
+         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Post $post ,PostRequest $request) 
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        // $post->title = $input["title"];
+        // $post->body = $input["body"];
+        // $post->save();fillによって短縮できる動き
+        return redirect('/posts/' . $post->id);
     }
 
 }
