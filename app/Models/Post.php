555c8e2562e5559763eab6a89889eb7dcdf587f7
U-ta122 <yuta8038@gmail.com>
2024-09-02 17:07:59 +0900

    8-02

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,11 +19,18 @@ class Post extends Model
     }
     public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        //return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 
     protected $fillable = [
         'title',
         'body',
+        'category_id'
     ];
 }
